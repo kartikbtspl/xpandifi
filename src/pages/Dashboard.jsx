0b91445612b5d28d73c68800d2053a9ca67c786e
kartikbtspl <kartik@brilliantechsoft.com>
2025-07-04 11:50:56 +0530

    merged all PR into release

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import StatCard from '../components/card/StatCard';
+import React from "react";
+import StatCard from "../components/card/StatCard";
+import ReusableTable from "../components/table/ReusableTable";
+import HeaderSection from "../components/ui/header-section/HeaderSection";
 
 const Dashboard = () => {
   const statsData = [
@@ -37,38 +39,37 @@ const Dashboard = () => {
     },
   ];
 
-//  const campaignData = Array(10).fill().map(() => ({
-//   image: "https://via.placeholder.com/30",
-//   campaign: "Diwali Sales for Beauty Products",
-//   slot: "11:00 AM  -  04:00 PM",
-//   bidAmount: 45000,
-//   bids: 256,
-//   status: "Active", // Try: "Canceled", "Rejected"
-//   resultDate: "12/06/2025",
-//   resultIn: "12 Days"
-// }));
+  //  const campaignData = Array(10).fill().map(() => ({
+  //   image: "https://via.placeholder.com/30",
+  //   campaign: "Diwali Sales for Beauty Products",
+  //   slot: "11:00 AM  -  04:00 PM",
+  //   bidAmount: 45000,
+  //   bids: 256,
+  //   status: "Active", // Try: "Canceled", "Rejected"
+  //   resultDate: "12/06/2025",
+  //   resultIn: "12 Days"
+  // }));
 
+  const columns = [
+    { id: "name", label: "Campaign Name" },
+    { id: "slot", label: "Slot" },
+    { id: "bidAmount", label: "Bid Amount", numeric: true },
+    { id: "bids", label: "Bids In Same Slot", numeric: true },
+    { id: "status", label: "Status" },
+    { id: "date", label: "Result In" },
+  ];
 
-const columns = [
-  { id: "name", label: "Campaign Name" },
-  { id: "slot", label: "Slot" },
-  { id: "bidAmount", label: "Bid Amount", numeric: true },
-  { id: "bids", label: "Bids In Same Slot", numeric: true },
-  { id: "status", label: "Status" },
-  { id: "date", label: "Result In" },
-];
-
-const rows = Array.from({ length: 10 }).map((_, i) => ({
-  id: i + 1,
-  image: campaignImg,
-  name: "Diwali Sales for Beauty Products",
-  slot: "11:00 AM - 04:00 PM",
-  bidAmount: 45000,
-  bids: 256,
-  status: i === 2 ? "Rejected" : i === 1 ? "Canceled" : "Active",
-  date: "12/06/2025",
-  daysLeft: "12 Days",
-}));
+  const rows = Array.from({ length: 10 }).map((_, i) => ({
+    id: i + 1,
+    image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww",
+    name: "Diwali Sales for Beauty Products",
+    slot: "11:00 AM - 04:00 PM",
+    bidAmount: 45000,
+    bids: 256,
+    status: i === 2 ? "Rejected" : i === 1 ? "Canceled" : "Active",
+    date: "12/06/2025",
+    daysLeft: "12 Days",
+  }));
 
   return (
     <div className="w-full">
@@ -79,7 +80,6 @@ const rows = Array.from({ length: 10 }).map((_, i) => ({
           buttonLabel="Create Campaign"
           onButtonClick={() => console.log("clicked")}
           subtitleClass="text-indigo-700 font-bold text-base"
-          
         />
       </div>
       <div
@@ -89,7 +89,9 @@ const rows = Array.from({ length: 10 }).map((_, i) => ({
         {statsData.map((item, index) => (
           <StatCard key={index} {...item} />
         ))}
-
+      </div>
+      <div className="mt-6">
+        <ReusableTable columns={columns} rows={rows} />
       </div>
     </div>
   );
