e520992a625083aa6872c5b0a82e9976d216dad1
kartikbtspl <kartik@brilliantechsoft.com>
2025-07-29 12:09:07 +0530

    deleting old code

--- src/routes/PublicRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Route } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
-import CreateCampaign from "../pages/campaign/CreateCampaign";
-import ContactUsForm from "../pages/AuthPages/ContactUsForm";
-import SignIn from "../pages/AuthPages/SignIn";
-export const publicRoutes = [
-  <Route key="signin" path="/signin" element={<SignIn />} />,
-  <Route path="/contact-us" element={<ContactUsForm />} />,
-]
