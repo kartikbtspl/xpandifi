d1997bce10ddb7dda7f678a0dd7f442dd7243f15
AvantikaBrilliantech <avantika@brilliantechsoft.com>
2025-08-06 12:42:11 +0530

    Updated Retailer dashboard. Created Campaigns,Reports sections

---
+++ src/pages/retailer/Devices.jsx
@@ -0,0 +1,10 @@
+const Devices = () => {
+
+    return (
+        <div>
+         <h2 className="text-xl font-semibold mb-4">Devices</h2>
+        </div>
+    )
+};
+
+export default Devices;
---
+++ src/pages/retailer/Wallet.jsx
@@ -0,0 +1,10 @@
+const Wallets = () => {
+
+    return (
+        <div>
+         <h2 className="text-xl font-semibold mb-4">Wallets</h2>
+        </div>
+    )
+};
+
+export default Wallets;
